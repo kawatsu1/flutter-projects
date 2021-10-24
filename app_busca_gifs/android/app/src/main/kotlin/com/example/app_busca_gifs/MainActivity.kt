d6e85fea652021e3ce9e85d5e68c8a949c81f5e9
André Kawatsu <kawatsu1066@gmail.com>
2021-10-24 01:08:52 -0300

    Aplicativo Buscador de Gif consumindo a API GIPHY Developers

---
+++ app_busca_gifs/android/app/src/main/kotlin/com/example/app_busca_gifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_busca_gifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ app_busca_gifs/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:app_busca_gifs/pages/gif_page.dart';
+import 'package:app_busca_gifs/pages/home_page.dart';
+//import 'package:app_busca_gifs/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:app_busca_gifs/pages/gif_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
